d590d1457ff922b6416590f651ae7dd243f023ea
EmiLy-Ly-san <emilie.deduyver@gmail.com>
2025-10-27 19:18:56 +0100

    feat/install: react vite ts tailwind is working

---
+++ ouest-creche/src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-pink-100">
+      <h1 className="text-4xl font-bold text-pink-700">
+        Tu crèches où 👶 ?
+      </h1>
+    </div>
+  );
+}
