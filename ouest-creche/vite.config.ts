fe7ae2c1c02a836cb19a8d879feeca6ecbe342eb
EmiLy-Ly-san <emilie.deduyver@gmail.com>
2025-10-30 16:39:01 +0100

    chore: config GitHub Pages

--- ouest-creche/vite.config.ts
+++ ouest-creche/vite.config.ts
@@ -1,18 +1,23 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Cette ligne indique à Vite où sera hébergée ton app sur GitHub Pages
+  base: '/ouest-creche/',
+
   plugins: [react(), tailwindcss()],
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
+
   server: {
     host: '0.0.0.0',
     port: 5173,
   },
-});
+})
